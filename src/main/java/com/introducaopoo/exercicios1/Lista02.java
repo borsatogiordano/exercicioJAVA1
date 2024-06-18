c48257b698c8b728c4a30562899d33f1ac65f607
Giordano Borsato <borsatogiordano@gmail.com>
2024-06-18 20:05:42 -0300

    Adição do Ex3

--- src/main/java/com/introducaopoo/exercicios1/Lista02.java
+++ src/main/java/com/introducaopoo/exercicios1/Lista02.java
@@ -22,6 +22,7 @@ public class Lista02 {
         else{
             System.out.println(numeroDigitado + " é 0");
         }
+        scanner.close();
     }
 
     public void ex2(){
@@ -41,8 +42,45 @@ public class Lista02 {
         } else {
             System.out.println(numeroDigitado1 + " é menor que " + numeroDigitado2);
         }
-
-
+        scanner.close();
     }
 
+    public void ex3(){
+        int respostaGestante;
+        int respostaPCD;
+        int respostaIdoso;
+        int preferencial = 0;
+
+        while (preferencial < 1){
+           
+            System.out.println("Boa noite, iremos fazer umas perguntas para encaminha-lo para sua fila de atendimento.\n\n Você é gestante?\n 1 - Sim \n 2 - Não");
+            respostaGestante = scanner.nextInt();
+            
+            if (respostaGestante == 1){
+                preferencial++;
+                break;
+            }
+
+            System.out.println("Você é PCD? \n 1 - Sim \n 2 - Não");
+            respostaPCD = scanner.nextInt();
+
+            if (respostaPCD == 1){
+                preferencial++;
+                break;
+            }
+
+            System.out.println("Você tem mais de 65 anos? \n 1 - Sim \n 2 - Não ");
+            respostaIdoso = scanner.nextInt();
+
+            if (respostaIdoso == 1){
+                preferencial++;
+                break;
+            }
+
+            System.out.println("Você está na fila comum!");
+            System.exit(0);
+        }
+        System.out.println("Você está na fila preferencial!");
+        scanner.close();
+    }
 }
